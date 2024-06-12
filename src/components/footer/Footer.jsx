8e0447c39e7d141dd775197b81d6605b937eb4dc
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-06-12 16:22:48 +0200

    footer finito

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,18 @@
-import footerStyles from './Footer.modules.scss'
+import footerStyles from './Footer.module.scss'
 
-const footer = () =>{
-    return(
-        <footer>
-            
+const Footer = () => {
+    return (
+        <footer className={footerStyles.bgBlack}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 text-center">
+                        <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, repellendus dignissimos! Debitis, quam. Dolorum eveniet unde aspernatur, nam eius iste! Nesciunt at asperiores similique blanditiis amet perferendis possimus quae nihil?</h5>
+
+                    </div>
+
+                </div>
+            </div>
+
         </footer>
     )
 }
